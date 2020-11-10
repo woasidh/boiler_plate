ac32c4262c425c016ba1a0aefdf3377f0b6820a5
woasidh <woasidh@ajou.ac.kr>
2020-11-10 20:41:41 +0900

    bcrypt password

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ app.post('/register', (req, res)=>{
     const user = new User(req.body)
 
     user.save((err, userInfo)=>{
-        if(err) return res.json({success: false, err})
+
+        if(err) {
+            return res.json({success: false, err})
+        }
         return res.status(200).json({success: true})
     })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -9,8 +11,33 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    //only when password is modified
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, (err, salt) =>{
+            if(err){
+                return next(err)
+            } 
+            bcrypt.hash(user.password, salt, (err, hash)=>{
+                if(err) {
+                    return next(err)
+                }
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
 const User = mongoose.model('User', userSchema)
+
 module.exports = {User}
