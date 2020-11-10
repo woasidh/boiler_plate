7f3b2e0ef3e758ac936cca9006df4003976da82c
woasidh <woasidh@ajou.ac.kr>
2020-11-10 16:49:33 +0900

    secure mongokey

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+const key = require('./config/key')
 const port = 5000
 
-mongoose.connect('mongodb+srv://minwoo:1q2w3e4r@react.u0nh5.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(key.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 })
 .then(() => console.log('mongodb connected!'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send(`Hello world!`))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send(`Hello world!ss`))
+
+app.post('/register', (req, res)=>{
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
+})
 
 app.listen(port, ()=>{
     console.log(`running on port${port}`)
